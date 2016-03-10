6a0929886cf889d75de2a5c1f5cbeabb79fa2d63
ScottHenry <scotthenry95@gmail.com>
2016-03-09 16:22:14 -0800

    Connected the database

--- Testing.php
+++ Testing.php
@@ -1,7 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title></title>
+</head>
+<body>
 <?php
-/**
- * Created by PhpStorm.
- * User: ScottHenry
- * Date: 2016-03-09
- * Time: 3:37 PM
- */
+
+$con = mysqli_connect("databasic.cvhyllwoxxb3.us-west-1.rds.amazonaws.com", "DataBasicTeam", "CPSC304!");
+if (!$con) {
+    exit('Connect Error (' . mysqli_connect_errno() . ') '
+        . mysqli_connect_error());
+}
+//set the default client character set
+mysqli_set_charset($con, 'utf-8');
+
+?>
+</body>
+</html>
+
+
+
+
