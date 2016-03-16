53293927a43e53a25e5de309e85b2a89c4a5e012
ScottHenry <scotthenry95@gmail.com>
2016-03-16 16:59:45 -0700

    Add a car

--- createrideshare.php
+++ createrideshare.php
@@ -2,3 +2,38 @@
 
 <?php
 require_once("db.php");
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    RideshareDB::getInstance()->insert_Car($_POST['icenseNum'], $_POST['type'], $_POST['color']);
+        exit;
+}
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+$car = array("licenseNum" => $_POST['licenseNum'],
+"type" => $_POST['type'],
+"color" => $_POST['color']);
+?>
+
+<form name="addCar" action="createrideshare.php" method="POST">
+     Num:
+    <input type="text" name="licenseNum" value="<?php echo $car['licenseNum']; ?>" />
+    Type:
+    <input type="text" name="type"  value="<?php echo $car['type']; ?>" />
+    Color:
+    <input type="text" name="color"  value="<?php echo $car['color']; ?>" />
+    <br/>
+
+    <input type="submit" name="saveCar" value="Save Changes"/>
+</form>
+</body>
+</html>
