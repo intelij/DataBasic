293fa5e5a34474cd1b76b8a247955d3f1b73dcd4
ScottHenry <scotthenry95@gmail.com>
2016-03-16 17:28:05 -0700

    Add a car

--- createrideshare.php
+++ createrideshare.php
@@ -5,10 +5,6 @@
 require_once("db.php");
 
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    RideshareDB::getInstance()->insert_Car($_POST['icenseNum'], $_POST['type'], $_POST['color']);
-        exit;
-}
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -19,10 +15,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 <body>
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "POST")
-$car = array("licenseNum" => $_POST['licenseNum'],
-"type" => $_POST['type'],
-"color" => $_POST['color']);
 
 $link = mysqli_connect('databasic.cvhyllwoxxb3.us-west-1.rds.amazonaws.com', 'DataBasicTeam', 'CPSC304!');
 if (!mysqli_ping($link)) {
@@ -31,16 +23,5 @@ if (!mysqli_ping($link)) {
 
 ?>
 
-<form name="addCar" action="createrideshare.php" method="POST">
-     Num:
-    <input type="text" name="licenseNum" value="<?php echo $car['licenseNum']; ?>" />
-    Type:
-    <input type="text" name="type"  value="<?php echo $car['type']; ?>" />
-    Color:
-    <input type="text" name="color"  value="<?php echo $car['color']; ?>" />
-    <br/>
-
-    <input type="submit" name="saveCar" value="Save Changes"/>
-</form>
 </body>
 </html>
---
+++ createuser.php
@@ -0,0 +1,46 @@
+<!-- CreateRideShare (Driver View) -->
+
+<?php
+
+require_once("db.php");
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    RideshareDB::getInstance()->insert_Car($_POST['icenseNum'], $_POST['type'], $_POST['color']);
+    exit;
+}
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST")
+    $car = array("licenseNum" => $_POST['licenseNum'],
+        "type" => $_POST['type'],
+        "color" => $_POST['color']);
+
+$link = mysqli_connect('databasic.cvhyllwoxxb3.us-west-1.rds.amazonaws.com', 'DataBasicTeam', 'CPSC304!');
+if (!mysqli_ping($link)) {
+    die('Not connected : ' . mysqli_error());
+}
+
+?>
+
+<form name="addCar" action="createuser.php" method="POST">
+    Num:
+    <input type="text" name="licenseNum" value="<?php echo $car['licenseNum']; ?>" />
+    Type:
+    <input type="text" name="type"  value="<?php echo $car['type']; ?>" />
+    Color:
+    <input type="text" name="color"  value="<?php echo $car['color']; ?>" />
+    <br/>
+
+    <input type="submit" name="saveCar" value="Save Changes"/>
+</form>
+</body>
+</html>
