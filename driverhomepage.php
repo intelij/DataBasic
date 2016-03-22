9f1fa6824b228512e1c654e8a76e71a7eacb2174
ScottHenry <scotthenry95@gmail.com>
2016-03-22 16:21:35 -0700

    Add a car

--- driverhomepage.php
+++ driverhomepage.php
@@ -1,3 +1,21 @@
 <?php
+session_start();
+if (array_key_exists("user", $_SESSION)) {
+    echo "Hello " . $_SESSION['user'];
+} else {
+    header('Location: index.php');
+    exit;
+}
+
 require_once("db.php");
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+</html>
+
 
