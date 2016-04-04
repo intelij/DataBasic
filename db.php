56adea7c9bdae44a42f0cafc14ae994ce4fce189
ScottHenry <scotthenry95@gmail.com>
2016-04-03 20:54:01 -0700

    Updated agregation

--- db.php
+++ db.php
@@ -286,7 +286,7 @@ class RideshareDB extends mysqli
     public function get_max_price_driver($driverID){
         $max_price = $this->query("SELECT MAX(price)
         FROM RideShare R, Driver D
-        WHERE D.DID = $driverID");
+        WHERE R.DID = ".$driverID);
 
         if ($max_price->num_rows > 0){
             $row = $max_price->fetch_row();
@@ -298,7 +298,7 @@ class RideshareDB extends mysqli
     public function get_ave_price_driver($driverID){
         $ave_price = $this->query("SELECT AVG(price)
         FROM RideShare R, Driver D
-        WHERE D.DID = $driverID");
+        WHERE R.DID = ".driverID);
         if ($ave_price->num_rows > 0){
             $row = $ave_price->fetch_row();
             return $row[0];
@@ -309,7 +309,7 @@ class RideshareDB extends mysqli
     public function get_min_price_driver($driverID){
         $min_price = $this->query("SELECT MIN(price)
         FROM RideShare R, Driver D
-        WHERE D.DID = $driverID");
+        WHERE R.DID =". $driverID);
 
         if ($min_price->num_rows > 0){
             $row = $min_price->fetch_row();
